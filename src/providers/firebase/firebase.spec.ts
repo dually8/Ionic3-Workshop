279a22c80cdda680a4a2aa21d2aad0cc55db86da
CJ Coffey <cjcoffey@tva.gov>
2017-07-18 15:21:17 -0400

    finished server spec

---
+++ src/providers/firebase/firebase.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
+import { FirebaseProvider } from './firebase';
+import { baseImports, baseProviders } from '../../../test-config/common-imports';
+
+fdescribe('FirebaseProvider', () => {
+
+  let testbed: TestBed;
+  let fb: FirebaseProvider;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+      ].concat(baseImports),
+      providers: [
+      ].concat(baseProviders)
+    });
+  }));
+
+  beforeEach(() => {
+    testbed = getTestBed();
+    fb = testbed.get(FirebaseProvider);
+  })
+
+
+
+  it('should be defined', async(() => {
+    expect(fb).toBeDefined();
+  }));
+
+  it('should login with email', async(() => {
+    fb.loginWithEmail('test@test.com', 'test123')
+      .then((user) => {
+        expect(user).toBeDefined();
+      })
+  }))
+});
